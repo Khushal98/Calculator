273e765aee2aab5b48c651e7580fdb3eb9fab356
Khushal98 <41577754+Khushal98@users.noreply.github.com>
2018-09-30 22:17:18 +0530

    use button for orientation change add button two button for history and converter in action bar

--- app/src/main/java/com/example/dell/calculator/MainActivity.java
+++ app/src/main/java/com/example/dell/calculator/MainActivity.java
@@ -8,7 +8,6 @@ import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
